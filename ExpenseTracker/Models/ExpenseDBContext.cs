26bb0f09e2c244e849d3e6f8378e6f1f4dfded2c
kutubuddinbd <developer.kutubuddin@gmail.com>
2021-12-10 22:52:27 +0600

    Add project files.

---
+++ ExpenseTracker/Models/ExpenseDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Models
+{
+    public class ExpenseDBContext : DbContext
+    {
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+        public ExpenseDBContext(DbContextOptions<ExpenseDBContext> options) : base(options)
+        {
+
+        }
+
+        public ExpenseDBContext()
+        {
+        }
+    }
+}
